76dc2e0bfabaac53ad1ff2fe7cba589a008ca464
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-04 15:28:26 -0700

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+    
+};
