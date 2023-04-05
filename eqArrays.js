1bcd90b46b48eb29166c73bd960e7db9adb5d45e
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-04 19:53:54 -0700

    Update eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+    
+};
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      return false;
+  }
+  return true;
+}
+  
+};
+
+
+
+//eqArrays([1, 2, 3], [1, 2, 3]) // => true
+//eqArrays([1, 2, 3], [3, 2, 1]) // => false
+//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
