7011b941f6614d290684d5f2f50bc5334986d23e
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-05 20:45:04 -0700

    Create findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKeyByValue = function(object, value) {
+  let objectKeys = Object.keys(object);
+  let index = 0;
+  for (let element in object) {
+    if (object[element] === value) {
+      return objectKeys[index];
+    }
+    index++;
+  }
+  return undefined;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  action: "The Last of Us"
+};
+
+// that 70's show and first love aren't listed, therefore are undefined.
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
+console.log(findKeyByValue(bestTVShowsByGenre, "First Love"));
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+console.log(findKeyByValue(bestTVShowsByGenre, "The Last of Us"));
+
