e2db68eae8c409a4aca4ce1f8decb2d641726601
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-05 20:16:22 -0700

    Update letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,29 @@
+const assertArraysEqual = function(array1, array2) {
+  if ((array1, array2)) {
+    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+  
+  for (let i = 0; i < sentence.length; i++) {
+     
+    if (results[sentence[i]] !== undefined) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+
+  }
+
   return results;
 };
+
+console.log(letterPositions('hello'));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
